874241fa2a7fa759b770156c9d942ed7a628f62c
Nolen Johnson <johnsonnolen@gmail.com>
2026-09-02 15:09:54 -0400

    radxa02: Update bootloader image prebuilts
    * Correct storeboot + update FIP toolchain to match.
    * Split SM1 board to it's own target.

--- Android.mk
+++ Android.mk
@@ -6,9 +6,9 @@ LOCAL_PATH := $(call my-dir)
 
 ifneq ($(filter radxa02 radxa02_car radxa02_tab,$(TARGET_DEVICE)),)
 
-$(call add-radio-file-sha1-checked,radio/bootloader-console.img,4b55386a77f8457c5dd4bf17560180090b14fd29)
-$(call add-radio-file-sha1-checked,radio/bootloader-recovery.img,23cc69398497ff4ec5f32668e7940bcb6d7a445b)
-$(call add-radio-file-sha1-checked,radio/bootloader.img,892339fe66a33534af0c42ae1fac171acdec06c9)
+$(call add-radio-file-sha1-checked,radio/bootloader-console.img,71a3de29e45e9b2ebd5a15432e322bcb26d32f18)
+$(call add-radio-file-sha1-checked,radio/bootloader-recovery.img,c09f6d7eac869f0bd58e42df1df241c88d378779)
+$(call add-radio-file-sha1-checked,radio/bootloader.img,de5a3ddb73608a2f271c11fa4debba5f39838b8b)
 $(call add-radio-file-sha1-checked,radio/misc.img,eb37860b2030f3d29ab61af22b96bae52bc7d42e)
 
 endif
