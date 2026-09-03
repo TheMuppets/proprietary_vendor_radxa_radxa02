2ae8b7427445eee1f180582373a1fcac56c1c022
Nolen Johnson <johnsonnolen@gmail.com>
2026-09-03 16:32:37 -0400

    radxa02: Update bootloader image prebuilts
    * Use G12A FIP.

--- Android.mk
+++ Android.mk
@@ -6,9 +6,9 @@ LOCAL_PATH := $(call my-dir)
 
 ifneq ($(filter radxa02 radxa02_car radxa02_tab,$(TARGET_DEVICE)),)
 
-$(call add-radio-file-sha1-checked,radio/bootloader-console.img,71a3de29e45e9b2ebd5a15432e322bcb26d32f18)
-$(call add-radio-file-sha1-checked,radio/bootloader-recovery.img,c09f6d7eac869f0bd58e42df1df241c88d378779)
-$(call add-radio-file-sha1-checked,radio/bootloader.img,de5a3ddb73608a2f271c11fa4debba5f39838b8b)
+$(call add-radio-file-sha1-checked,radio/bootloader-console.img,6369f7be9f5c0c1e2897f292e0c6397b062d98e8)
+$(call add-radio-file-sha1-checked,radio/bootloader-recovery.img,6bd9fd0dc867794d65a0eacb1daea7c06257c933)
+$(call add-radio-file-sha1-checked,radio/bootloader.img,181d11b6b92fad03cd1209b788fd0c1223cac85c)
 $(call add-radio-file-sha1-checked,radio/misc.img,eb37860b2030f3d29ab61af22b96bae52bc7d42e)
 
 endif
